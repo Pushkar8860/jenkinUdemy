13b9e64b9a3b54a05d7bd468aeb373f28912d095
pushkar <pushkar8860@gmail.com>
2022-10-26 09:56:43 +0530

    add test method

--- jenkins/src/main/java/com/example/App.java
+++ jenkins/src/main/java/com/example/App.java
@@ -12,6 +12,10 @@ public class App
     {
         System.out.println("Hello fron greeting ");
     }   
+    public void test()
+    {
+        System.out.println("Hello fron test ");
+    }   
  public static void main( String[] args )
     {
         System.out.println( "Hello World!  Main Method" );
